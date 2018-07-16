5d60888e3ddc6c7252862be511b89dedc243ba4a
ronald vilorio <soccer23415@gmail.com>
2018-07-16 16:29:01 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -110,7 +110,7 @@ function previousGames(){
 
 function saveGame(){
   if(currentGame === 0){
-    $.post("/games",$("td").text()).done(function(response){
+    $.post("/games",{"state": $("td")}).done(function(response){
       // debugger
       currentGame = response.data.id
     })

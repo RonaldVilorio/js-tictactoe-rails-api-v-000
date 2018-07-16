cc34a2d9f833e7965706fc02bb11a8fb29f4dbd2
ronald vilorio <soccer23415@gmail.com>
2018-07-16 17:15:33 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -85,7 +85,7 @@ function attachListeners(){
       method: "GET"
     }).done(function(response){
       response.data.attributes.state.forEach(function(i,val){
-        $(`td:eq(${})`).html(i)
+        $(`td:eq(${})`).html(val)
       })
       // // $("td").each(function(){
       // //   $(this).text += response.data.attributes.state
